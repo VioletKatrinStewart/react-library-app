40d9d1e517492e24e39c12bcfbfc9805230c673c
Violet Stewart <vkazoo2@gmail.com>
2021-12-16 15:43:23 -0800

    added route to books

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import BookList from './views/Books/BookList';
+import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
+// import BookDetail from './views/Books/BookDetail';
 import './App.css';
 
 function App() {
   // TODO: Add routes to books & views
+
   return (
     <main className="container">
-      <h1>Library Catalog</h1>
-      <BookList />
+      <BrowserRouter>
+        <h1>Library Catalog</h1>
+        <Switch>
+          <Route path="/books" exact component={BookList} />
+        </Switch>
+        {/* one route just / 
+            one route is /books
+            one route is /books/:id
+
+            I will need a link to catalog from home page 
+            */}
+      </BrowserRouter>
     </main>
   );
 }
